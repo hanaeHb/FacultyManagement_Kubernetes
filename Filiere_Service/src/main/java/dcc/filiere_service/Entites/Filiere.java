a245298b303dc8478f9063b49347ccd01b5e4974
hanae <hanaebertili@gmail.com>
2025-12-12 23:29:38 +0100

    first commit

---
+++ Filiere_Service/src/main/java/dcc/filiere_service/Entites/Filiere.java
@@ -0,0 +1,19 @@
+package dcc.filiere_service.Entites;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@ToString
+public class Filiere {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer idFiliere;
+    private String code;
+    private String libelle;
+}
---
+++ Filiere_Service/src/main/java/dcc/filiere_service/FiliereServiceApplication.java
@@ -0,0 +1,16 @@
+package dcc.filiere_service;
+
+import dcc.filiere_service.configuration.RsaKeys;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(RsaKeys.class)
+public class FiliereServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiliereServiceApplication.class, args);
+    }
+
+}
